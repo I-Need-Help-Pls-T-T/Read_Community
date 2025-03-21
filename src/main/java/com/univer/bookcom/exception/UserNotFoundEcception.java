952b574ccff3b8f0655f6a2bdd7391516fd93a42
I-Need-Help-Pls-T-T <nagat.mariya@gmail.com>
2025-03-21 09:12:02 +0300

    Delete src/main/java/com/univer/bookcom/exception/UserNotFoundEcception.java

--- src/main/java/com/univer/bookcom/exception/UserNotFoundEcception.java
+++
@@ -1,7 +0,0 @@
-package com.univer.bookcom.exception;
-
-public class UserNotFoundEcception extends RuntimeException {
-    public UserNotFoundEcception(String message) {
-        super(message);
-    }
-}
