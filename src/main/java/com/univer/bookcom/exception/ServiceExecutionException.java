94493ddd70c8e7af2097e6f487f3837ce3e6fdf1
I-Need-Help-Pls-T-T <nagat.mariya@gmail.com>
2025-05-27 12:27:20 +0300

    Delete src directory

--- src/main/java/com/univer/bookcom/exception/ServiceExecutionException.java
+++
@@ -1,7 +0,0 @@
-package com.univer.bookcom.exception;
-
-public class ServiceExecutionException extends RuntimeException {
-    public ServiceExecutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
