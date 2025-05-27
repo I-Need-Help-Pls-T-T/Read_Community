4275e735c86859060a60cfb6d6c05c916e17e6c0
I-Need-Help-Pls-T-T <nagat.mariya@gmail.com>
2025-05-27 12:29:24 +0300

    Delete main directory

--- main/java/com/univer/bookcom/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.univer.bookcom.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
