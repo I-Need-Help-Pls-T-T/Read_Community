042743e772ebe411262ba7c9fe19a27ec377cecf
I-Need-Help-Pls-T-T <nagat.mariya@gmail.com>
2025-05-28 19:36:55 +0300

    Delete src directory

--- src/main/java/com/univer/bookcom/exception/DuplicateCommentException.java
+++
@@ -1,7 +0,0 @@
-package com.univer.bookcom.exception;
-
-public class DuplicateCommentException extends RuntimeException {
-    public DuplicateCommentException(String message) {
-        super(message);
-    }
-}
